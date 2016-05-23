1874e3ea26d82870160f76cb699a84734e97c706
yu <yuya4.matsumura@gmail.com>
2016-05-23 17:27:16 +0900

    add matrix factorization function

--- collaborativefilterng.py
+++ collaborativefilterng.py
@@ -20,10 +20,6 @@ class CollaborativeFiltering:
         """
         行列を特異値分解(SVD)する
 
-        Args:
-            mat: numpy.matrix
-                SVDする行列
-
         Returns:
             List<numpy.matrix>
             特異値分解された行列3つ
@@ -45,12 +41,6 @@ class CollaborativeFiltering:
     def lsa(self, k):
         """
         行列にLSAを適用する
-
-        Args:
-            mat: numpy.matrix
-                LSAを適用する行列
-            k: int
-                圧縮する次元数
 
         Returns:
             numpy.matrix
@@ -78,9 +68,6 @@ class CollaborativeFiltering:
         """
         行列にProbabilistic Matrix Factorizationを適用する
 
-        Args:
-            mat: numpy.matrix
-                PMFを適用する行列
         Returns:
             適用結果
         """
@@ -89,9 +76,39 @@ class CollaborativeFiltering:
         """
         行列にFactorization Machineを適用する
 
-        Args:
-            mat: numpy.matrix
-
         Returns:
             適用結果
         """
+    def get_rating_error(self, r, p, q):
+        return r - np.dot(p, q)
+
+
+    def get_error(self, R, P, Q, beta):
+        error = 0.0
+        for i in range(len(R)):
+            for j in range(len(R[i])):
+                if R[i][j] == 0:
+                    continue
+                error += pow(self.get_rating_error(R[i][j], P[:,i], Q[:,j]), 2)
+        error += beta/2.0 * (np.linalg.norm(P) + np.linalg.norm(Q))
+        return error
+
+
+    def matrix_factorization(self, K, steps=5000, alpha=0.0002, beta=0.02, threshold=0.001):
+        R = np.array(self.matrix)
+        P = np.random.rand(K, len(R))
+        Q = np.random.rand(K, len(R[0]))
+
+        for step in range(steps):
+            for i in range(len(R)):
+                for j in range(len(R[i])):
+                    if R[i][j] == 0:
+                        continue
+                    err = self.get_rating_error(R[i][j], P[:, i], Q[:, j])
+                    for k in range(K):
+                        P[k][i] += alpha * (2 * err * Q[k][j])
+                        Q[k][j] += alpha * (2 * err * P[k][i])
+            error = self.get_error(R, P, Q, beta)
+            if error < threshold:
+                break
+        return P, Q
