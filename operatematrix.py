1540aafb3d82a3b52c11eeaff0c383478b604fef
yu <yuya4.matsumura@gmail.com>
2016-05-23 15:57:15 +0900

    remove collaborative filtering function

--- operatematrix.py
+++ operatematrix.py
@@ -5,88 +5,7 @@ import copy
 import math
 import numpy as np
 from scipy import linalg
-
-def svd(mat):
-    """
-    行列を特異値分解(SVD)する
-
-    Args:
-        mat: numpy.matrix
-            SVDする行列
-
-    Returns:
-        List<numpy.matrix>
-        特異値分解された行列3つ
-        特異値はリストで返される
-    """
-    #mat = np.matrix(mat)
-
-    """
-    full_matrices:
-        1: UとVが正方行列に(次元が合わずに死ぬ??)
-        0: UとVのかたちをいい感じに(とりあえずこれでなんとかしてる)
-    """
-    U, s, V = np.linalg.svd(mat, full_matrices=0)
-    #print(s)
-
-    return [U, s, V]
-
-
-def lsa(mat, k):
-    """
-    行列にLSAを適用する
-
-    Args:
-        mat: numpy.matrix
-            LSAを適用する行列
-        k: int
-            圧縮する次元数
-
-    Returns:
-        numpy.matrix
-        LSAを適用した結果
-    """
-    rank = np.linalg.matrix_rank(mat)
-    # ランク以上の次元数を指定した場合は，ランク数分の特徴量を使用
-    #npの仕様上，ランク以上分の特徴量を算出してるっぽい？
-    # 負の値が入力された場合はk = 0 とする
-    U, s, V = svd(mat)
-    if k > rank:
-        k = rank
-    if k < 0:
-        k = 0
-
-    #print("ランクk=%d 累積寄与率=%f" % (k, sum(s[:k]) / sum(s)))
-    S = np.zeros((len(s),len(s)))
-    S[:k, :k] = np.diag(s[:k]) #上からk個の特異値のみを使用
-
-    lsa_mat = np.dot(U, np.dot(S, V))
-
-    return lsa_mat
-
-def probabilistic_matrix_factorization(mat):
-    """
-    行列にProbabilistic Matrix Factorizationを適用する
-
-    Args:
-        mat: numpy.matrix
-            PMFを適用する行列
-    Returns:
-        適用結果
-    """
-
-def factorization_machine(mat):
-    """
-    行列にFactorization Machineを適用する
-
-    Args:
-        mat: numpy.matrix
-
-    Returns:
-        適用結果
-    """
-
-
+from collaborativefilterng import CollaborativeFiltering
 
 def read_act_geoclass_matrix():
     """
@@ -291,30 +210,31 @@ def evaluate_geoclass(act, mat, k):
 
 if __name__ == '__main__':
 
-    # mat = [[5, 0, 0, 0],
-    #         [3, 4, 0, 0],
-    #         [2, 0, 1, 0],
-    #         [2, 4, 0, 3],
-    #         [0, 5, 0, 4],
-    #         [0, 0, 0, 5]]
-    #
-    #
-    # # print(type(mat))
-    # mat = np.matrix(mat)
-    # #t = mat.T
-    # #print(t)
-    # # print(type(mat))
-    # lsa_mat = lsa(mat, 2)
-    #
-    # #print(lsa_mat)
-    #
-    # #lsa_matt = lsa(t, 2)
-    #
-    # #print(lsa_matt)
-    # exit()
+    mat = [[5, 0, 0, 0],
+            [3, 4, 0, 0],
+            [2, 0, 1, 0],
+            [2, 4, 0, 3],
+            [0, 5, 0, 4],
+            [0, 0, 0, 5]]
+
+
+    mat = np.matrix(mat)
+
+    cf = CollaborativeFiltering(mat)
+
+
+    lsa_mat = cf.lsa(2)
+
+    print(lsa_mat)
+
+    #lsa_matt = lsa(t, 2)
+
+    #print(lsa_matt)
+    exit()
 
     act_geoclass_mat = read_act_geoclass_matrix()
     act_geoclass_mat = np.matrix(act_geoclass_mat)
+    cf = CollaborativeFiltering(act_geoclass_mat)
 
     k = input()
     k = int(k)
