a8ec9b0176dbc49852a4ba086302b96daaaec87e
yu <yuya4.matsumura@gmail.com>
2016-06-10 16:30:18 +0900

    add test case

--- fmtest.py
+++ fmtest.py
@@ -2,23 +2,38 @@ import pywFM
 import numpy as np
 import pandas as pd
 
+"""
+export LIBFM_PATH=/usr/local/libfm/bin/
+"""
 if __name__ == '__main__':
 
+
+    # features = np.matrix([
+    # # Users | Movies | Movie Ratings | Time | Last Movies Rated
+    # # A B C | TI NH SW ST | TI NH SW ST | | TI NH SW ST
+    # [1, 0, 0, 1, 0, 0, 0, 0.3, 0.3, 0.3, 0, 13, 0, 0, 0, 0 ],
+    # [1, 0, 0, 0, 1, 0, 0, 0.3, 0.3, 0.3, 0, 14, 1, 0, 0, 0 ],
+    # [1, 0, 0, 0, 0, 1, 0, 0.3, 0.3, 0.3, 0, 16, 0, 1, 0, 0 ],
+    # [0, 1, 0, 0, 0, 1, 0, 0, 0, 0.5, 0.5, 5, 0, 0, 0, 0 ],
+    # [0, 1, 0, 0, 0, 0, 1, 0, 0, 0.5, 0.5, 8, 0, 0, 1, 0 ],
+    # [0, 0, 1, 1, 0, 0, 0, 0.5, 0, 0.5, 0, 9, 0, 0, 0, 0 ],
+    # [0, 0, 1, 0, 0, 1, 0, 0.5, 0, 0.5, 0, 12, 1, 0, 0, 0 ]
+    # ])
+    # target = [5, 3, 1, 4, 5, 0, 0]
 
     features = np.matrix([
     # Users | Movies | Movie Ratings | Time | Last Movies Rated
     # A B C | TI NH SW ST | TI NH SW ST | | TI NH SW ST
-    [1, 0, 0, 1, 0, 0, 0, 0.3, 0.3, 0.3, 0, 13, 0, 0, 0, 0 ],
-    [1, 0, 0, 0, 1, 0, 0, 0.3, 0.3, 0.3, 0, 14, 1, 0, 0, 0 ],
-    [1, 0, 0, 0, 0, 1, 0, 0.3, 0.3, 0.3, 0, 16, 0, 1, 0, 0 ],
-    [0, 1, 0, 0, 0, 1, 0, 0, 0, 0.5, 0.5, 5, 0, 0, 0, 0 ],
-    [0, 1, 0, 0, 0, 0, 1, 0, 0, 0.5, 0.5, 8, 0, 0, 1, 0 ],
-    [0, 0, 1, 1, 0, 0, 0, 0.5, 0, 0.5, 0, 9, 0, 0, 0, 0 ],
-    [0, 0, 1, 0, 0, 1, 0, 0.5, 0, 0.5, 0, 12, 1, 0, 0, 0 ]
+    [1, 0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 1, 0],
+    [1, 0, 0, 0, 0, 1],
+    [0, 1, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0, 1]
     ])
-    target = [5, 3, 1, 4, 5, 1, 5]
+    target = [2, 1, 1, 2, 1, 1111110]
 
-    fm = pywFM.FM(task='regression', num_iter=5)
+    fm = pywFM.FM(task='regression', num_iter=1000)
 
     # split features and target for train/test
     # first 5 are train, last 2 are test
