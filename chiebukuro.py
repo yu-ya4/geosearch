80132b8574d04bd3de3ea818caa289ce88db009d
yu <yuya4.matsumura@gmail.com>
2016-07-07 20:52:34 +0900

    extract verb and object

--- chiebukuro.py
+++ chiebukuro.py
@@ -4,6 +4,7 @@
 import requests
 import json
 from elasticsearch import Elasticsearch
+from cabocha_matcher import CaboChaMatcher
 
 class Chiebukuro():
     def __init__(self):
@@ -22,7 +23,7 @@ class Chiebukuro():
             questionのlist
             json形式
         '''
-        json = {"size": 10,"query":{"query_string":{"analyzer": "ngram_analyzer","query": "\"場所\"", "fields" : ["body", "title"]}}}
+        json = {"size": 1000,"query":{"query_string":{"analyzer": "ngram_analyzer","query": "\"場所\"", "fields" : ["body", "title"]}}}
 
         res = self.es.search(index=self.index, doc_type='questions', body=json)
 
@@ -40,8 +41,119 @@ class Chiebukuro():
         Returns:
             action
         '''
+        res = []
+        pattern_num = 23
+        except_verbs = ['行く', 'いく', '来る', 'くる', '着く', 'つく', '訪れる', 'できる', '有る', '在る',
+                        'ある', 'なる', 'いる', '住む', '言う', 'いう', '聞く', 'きく', 'する', '思う']
 
-        return que_body
+        matcher = CaboChaMatcher()
+        pat = []
+        pattern = []
+        texts = self.text_split(que_body)
+
+        for i in range(0, pattern_num):
+            file_name = "pattern/get_act/qa/act_qa_" + str(i) + ".txt"
+            with open(file_name) as f:
+                pat.append(f.read())
+            pattern.append(matcher.parse_pat(pat[i]))
+
+        for text in texts:
+            sentence = matcher.parse(text)
+
+            exacted = 0
+            for i in range(0, pattern_num):#パターンとのマッチング
+
+                if exacted: #抽出されたらその時点で終了
+                    break
+                else:
+
+                    results = matcher.match(sentence, pattern[i])
+
+                    if results != None:
+                        act = []
+                        for result in results:
+                            if len(result) == 2:
+                                #動作の対象に'場所'を抽出した場合は無視
+                                if result[0][0].dictform == '場所':
+                                    continue
+                                elif i == 8 or i == 9:
+                                    act.append(result[0][0].dictform + result[1][0].dictform)#対象
+                                    act.append('する') #動作
+                                elif result[1][0].dictform in except_verbs:
+                                    continue
+                                else:
+                                    act.append(result[0][0].dictform) #対象
+                                    act.append(result[1][0].dictform) #動作
+                            elif len(result) == 1:
+                                if i == 2 or i == 3:
+                                    act.append(result[0][0].dictform)#対象
+                                    act.append('みる') #動作
+                                elif result[0][0].pos == '名詞' and result[0][0].detailed_pos[0] == '一般':
+                                    ''' [名詞]する場所，[名詞]できる場所，[名詞]ができる場所 パターンの処理'''
+                                    act.append(result[0][0].dictform)#対象
+                                    act.append('する') #動作
+                                elif result[0][0].dictform in except_verbs:
+                                    continue
+                                else:
+                                    act.append(None)#対象
+                                    act.append(result[0][0].dictform) #動作
+                            act.append(self.qid)
+                            res.append(act)
+
+                            exacted = 1
+                            '''
+                            print( self.text)
+                            print('\n')
+                            for ans in self.ans:
+                                print(ans.text)
+                                print('\n')
+                            print(act)
+                            print('---------------------------------------\n')
+                            '''
+
+                            break
+
+
+        return res
+
+    def text_split(self, text):
+        '''
+        一部の表現をマッチングしやすくする
+        記号で区切り， "場所"という単語を含む文を判定
+        Args:
+            text: str
+        Returns:
+            list[str]
+        '''
+        res = []
+        syms = ['.', '。', ',', '，', '、', '?', '？', '!', '！']
+        text = text.replace("おすすめの", "")
+        text = text.replace("オススメの", "")
+        text = text.replace("お勧めの", "")
+        text = text.replace("お薦めの", "")
+
+        #text = text.replace("いい場所", "場所")
+        #text = text.replace("良い場所", "場所")
+        #text = text.replace("適した場所", "場所")
+        text = text.replace("見える", "みれる")
+        text = text.replace("観れる", "みれる")
+        text = text.replace("見れる", "みれる")
+        text = text.replace("出来る", "できる")
+
+        print(text)
+        for sym in syms:
+            text = text.replace(sym, " ")
+
+        texts = text.split()
+
+        for text in texts:
+            if '場所' in text:
+                res.append(text)
+
+        return res
+
+
+
 
     def get_answers(self, question_ids):
         '''
@@ -51,6 +163,8 @@ class Chiebukuro():
             list[dict[str, str or int]]
             answerのlist
             json形式
+
+            ないやつ指定した際の処理もおk
         '''
         ids = ''
         for question_id in question_ids:
@@ -85,20 +199,19 @@ class Chiebukuro():
                 body = title + body
 
             # print(str(question_id) + ': ' + str(body))
-            action = self.extract_action(body)
-            if action in action_dict:
-                action_dict[action].append(question_id)
-            else:
-                action_dict[action] = [question_id]
-
+            actions = []
+            actions = self.extract_action(body)
+            for action in actions:
+                if action in action_dict:
+                    action_dict[action].append(question_id)
+                else:
+                    action_dict[action] = [question_id]
         return action_dict
 
 
 
 if __name__ == '__main__':
     chie = Chiebukuro()
-    answers = chie.get_answers([148926454,148926564])
-    exit()
     questions = chie.search_questions("場所")
     action_dict = chie.make_action_dict(questions)
     print(action_dict)
